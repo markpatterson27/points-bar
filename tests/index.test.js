21a7c174b436d182f00f46586c35daed8487bab5
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-09-18 09:23:08 +0100

    added jest package and initial test setup

---
+++ tests/index.test.js
@@ -0,0 +1,10 @@
+const process = require('process');
+const cp = require('child_process');
+// const path = require('path');
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  // const ip = path.join(__dirname, '..', 'src', 'index.js');
+  const ip = 'src/index.js';
+  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+})
