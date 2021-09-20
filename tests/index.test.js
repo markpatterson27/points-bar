747119a328e8348058902ccd36f3098ce0a0eec3
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2021-09-20 05:18:15 +0100

    added inputs to runner test

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,6 +4,8 @@ const cp = require('child_process');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
+  process.env['INPUT_POINTS'] = '12/34';
+  process.env['INPUT_PATH'] = '.github/icons/pointsbar.svg';
   // const ip = path.join(__dirname, '..', 'src', 'index.js');
   const ip = 'src/index.js';
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
